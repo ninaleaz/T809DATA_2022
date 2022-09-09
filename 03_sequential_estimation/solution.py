0e77735a7d78539450808a8664771838987c7207
ninaleaz <32267536+ninaleaz@users.noreply.github.com>
2022-09-09 10:53:36 +0000

    04 classification
    NLZJ

--- 03_sequential_estimation/solution.py
+++ 03_sequential_estimation/solution.py
@@ -2,9 +2,9 @@
 # Date: 02.09.2022
 # Project: Sequential Estimation
 # Acknowledgements: 
+# 1.2 section collaborated with Einar and Magnea
 # Independent section collaborated with Arnbjörg
 # ATH vantar:
-#       1.2 pdf form
 #       ræða um idependent section
 
 
@@ -72,101 +72,16 @@ def _plot_mean_square_error():
     plt.plot([e for e in avg_error])
     plt.show()
 
-
-# Naive solution to the independent question.
-
-def gen_changing_data(
-    n: int,
-    k: int,
-    start_mean: np.ndarray,
-    end_mean: np.ndarray,
-    var: float
-) -> np.ndarray:
-
-    d = np.ndarray(shape=(n, k))
-    var = np.identity(k) * np.power(var, 2)
-    mean = np.linspace(start_mean, end_mean, num = n)
-    for i in range(n):
-        d[i] = np.random.multivariate_normal(mean[i], var, 1)
-    return d, mean
-
-
-def weighted_update_sequence_mean(
-    mu: np.ndarray,
-    x: np.ndarray,
-    n: int,
-    l: int
-) -> np.ndarray:
-    mu_update = mu + (x-mu)*(1/n + 1/l)
-    return np.asarray(mu_update)
-
-
-def _plot_changing_sequence_estimate():
-    estimates = [np.array([0, 0, 0])]
-    avg_error = []
-    data, true_mean = gen_changing_data(500, 3, [0, 1, -1], [1, -1, 0], 1)
-
-    for i in range(data.shape[0]):
-        e = weighted_update_sequence_mean(estimates[i], data[i], i + 1,50)
-        estimates.append(e)
-        a_error = np.mean(_square_error(true_mean[i], e))
-        avg_error.append(a_error)
-
-    plt.figure(0)
-    plt.plot([e[0] for e in estimates], label='First dimension')
-    plt.plot([e[1] for e in estimates], label='Second dimension')
-    plt.plot([e[2] for e in estimates], label='Third dimension')
-
-    plt.legend(loc='upper center')
-    plt.show()
-
-    plt.figure(1)
-    plt.plot([e for e in avg_error])
-    plt.show()
-
-# #hann prófaði í tíma:
-# # 1.1
-# mean = [0,0]
-# cov = [[1,0], [0,100]]
-# # ef við gerum þetta 2x þá koma mismunandi tölur því þetta er randnom
-# np.random.multivariate_normal(mean,cov,10)
-# # nú fæ ég sömu tölur og áðan með þessari skipun
-# # seed er til að prófa, viljum það svosem ekki
-# np.random.seed(1234)
-# np.random.multivariate_normal(mean,cov,10)
-
-# # 1.4
-# # mu = mean
-
-# # from teacher
-
-# from scipy.stats import multivariable_normal
-# rv = multivariable_normal([0.5,-0.2],[[2.0,0.3],[0.3,0.5]])
-# rv
-# # gives us the right thing
-# rv.
-
-# rv.pdf([2.2,-1.2])
-# rv.pdf([1.1,-0.2])
-# rv.pdf([0.5,-0.2])
-
-# np.identity(5)
-
-# x = np.identity(5)
-# x
-# x.T
-
-
 if __name__ == '__main__':
 
     # 1.1
     #print(gen_data(2, 3, np.array([0, 1, -1]), 1.3))
-    X = gen_data(5, 1, np.array([0.5]), 0.5)
+    X = gen_data(300, 3, np.array([0,1,-1]), np.sqrt(3))
     #print(X)
 
     # 1.2
-    #scatter_3d_data(X)
-    #bar_per_axis(X)
+    scatter_3d_data(X)
+    bar_per_axis(X)
 
     # 1.4
     mean = np.mean(X, 0)
@@ -178,7 +93,4 @@ if __name__ == '__main__':
 
     # 1.6
     #_plot_mean_square_error()
-
-    # Independent Section
-    _plot_changing_sequence_estimate()
 
--- 04_classification/solution.py
+++ 04_classification/solution.py
@@ -2,9 +2,10 @@
 # Date: 07.09.22
 # Project: Classification
 # Acknowledgements: 
-# 1.1 collaborated with Magnea
+# 1.1, 1.2 collaborated with Magnea
 
 
+from decimal import ConversionSyntax
 from tools import load_iris, split_train_test
 
 import matplotlib.pyplot as plt
@@ -25,8 +26,9 @@ def mean_of_class(
     for i in range(len(targets)):
         if targets[i] == selected_class:
             class_features.append(features[i])
+
     mean = np.mean(class_features, axis = 0)
-    print(mean)
+
     return mean
 
 
@@ -39,7 +41,14 @@ def covar_of_class(
     Estimate the covariance of a selected class given all
     features and targets in a dataset
     '''
-    ...
+    class_features = []
+    for i in range(len(targets)):
+        if targets[i] == selected_class:
+            class_features.append(features[i])
+
+    covar = np.cov(class_features, rowvar = False)
+
+    return covar
 
 
 def likelihood_of_class(
@@ -52,7 +61,8 @@ def likelihood_of_class(
     from a multivariate normal distribution, given the mean
     and covariance of the distribution.
     '''
-    ...
+    likelihood = multivariate_normal(mean = class_mean, cov=class_covar,allow_singular=True).pdf(feature)
+    return likelihood
 
 
 def maximum_likelihood(
@@ -72,10 +82,16 @@ def maximum_likelihood(
     '''
     means, covs = [], []
     for class_label in classes:
-        ...
+        means.append(mean_of_class(train_features, train_targets, class_label))
+        covs.append(covar_of_class(train_features, train_targets, class_label))
+    
     likelihoods = []
     for i in range(test_features.shape[0]):
-        ...
+        class_likelihoods = []
+        for j in range(len(classes)):
+            class_likelihoods.append(likelihood_of_class(test_features[i],means[j],covs[j]))
+        likelihoods.append(class_likelihoods)
+
     return np.array(likelihoods)
 
 
@@ -88,7 +104,12 @@ def predict(likelihoods: np.ndarray):
     You should return a [likelihoods.shape[0]] shaped numpy
     array of predictions, e.g. [0, 1, 0, ..., 1, 2]
     '''
-    ...
+    class_pred = []
+    for i in range(likelihoods.shape[0]):
+        pred = np.argmax(likelihoods[i])
+        class_pred.append(pred)
+
+    return np.array(class_pred)
 
 
 def maximum_aposteriori(
@@ -106,34 +127,68 @@ def maximum_aposteriori(
     a [test_features.shape[0] x len(classes)] shaped numpy
     array
     '''
-    ...
+    means, covs, likelihoods, a_post = [], [], [], []
+    for class_label in classes:
+        means.append(mean_of_class(train_features, train_targets, class_label))
+        covs.append(covar_of_class(train_features, train_targets, class_label))
+        likelihoods.append((np.count_nonzero(train_targets == class_label)/train_targets.shape[0]))
+    #print(likelihoods)
+    for i in range(test_features.shape[0]):
+        class_likelihoods = []
+        for j in range(len(classes)):
+            class_likelihoods.append(likelihoods[j]*likelihood_of_class(test_features[i],means[j],covs[j]))
+        a_post.append(class_likelihoods)
 
-    # MAIN
+    return np.array(a_post)
+
+def accuracy(
+    likelihoods: np.ndarray,
+    test_targets: np.ndarray,
+) -> float:
+    pred = predict(likelihoods)
+    return np.sum(pred == test_targets)/len(test_targets)
+
+def confusion_matrix(
+    likelihoods: np.ndarray,
+    test_targets: np.ndarray,
+    classes: list,
+) -> np.ndarray:
+
+    prediction = predict(likelihoods)
+    matrix = np.zeros((len(classes), len(classes)))
+    for pred, targ in zip(prediction, test_targets):
+        matrix[pred][targ] += 1
+    return matrix
+
+
+if __name__ == '__main__':
 
     # 1.1
     features, targets, classes = load_iris()
     (train_features, train_targets), (test_features, test_targets) = split_train_test(features, targets, train_ratio=0.6)
+    #print(mean_of_class(train_features, train_targets, 0))
 
-    # teacher:
-    # 1.1
-    # split train test is using random
+    # 1.2
+    #print(covar_of_class(train_features, train_targets, 0))
 
     # 1.3
-    # one point one number, gives output: likeleehood of this point is in what class
+    class_mean = mean_of_class(train_features, train_targets, 0)
+    class_cov = covar_of_class(train_features, train_targets, 0)
+    #print(likelihood_of_class(test_features[0, :], class_mean, class_cov))
 
     # 1.4
-    # for every datapoint
-    # given the vector we get, what class is the output likeliest to be?
+    #print(maximum_likelihood(train_features, train_targets, test_features, classes))
 
     # 1.5 
-    # how likely is it that this point comes from this distribution
+    likelihoods1 = maximum_likelihood(train_features, train_targets, test_features, classes)
+    #print(predict(likelihoods1))
 
     # 2.1
-    # adding probabilty of each class (the extra formula)
-    # we are scaling the lielyhood with this number
+    likelihoods2 = maximum_aposteriori(train_features, train_targets, test_features, classes)
+    #print(likelihoods2)
 
     # 2.2
-    # pdf svara spurningum
-
-    # independent
-    # posteriori should give us better resaults
+    print(accuracy(likelihoods1,test_targets))
+    print(accuracy(likelihoods2,test_targets))
+    print(confusion_matrix(likelihoods1,test_targets, classes))
+    print(confusion_matrix(likelihoods2,test_targets, classes))
